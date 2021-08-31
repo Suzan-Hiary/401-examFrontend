90ea64989a32fd1ea518ac960e6de13bc9e8c1ca
Suzan-Hiary <suze-hiary@hotmail.com>
2021-08-31 15:49:20 +0300

    added

--- src/components/FavCrypto.js
+++ src/components/FavCrypto.js
@@ -1,11 +1,120 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, Button, Modal, Form } from 'react-bootstrap'
+import axios from 'axios';
 
 class FavCrypto extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      favArray: [],
+      title: '',
+      description: '',
+      toUSD: '',
+      image: '',
+      showModal :false
+    }
+  }
+
+  componentDidMount = () => {
+
+
+    const requestconfig = {
+      method: 'get',
+      baseurl: process.env.REACT_APP_URL,
+      url: '/fav-Crypto',
+
+    }
+
+    axios(requestconfig).then(response => {
+      this.setState({
+        favArray: response
+      })
+    })
+  }
+
+
+  deletefav = (items, id) => {
+    axios.delete(`${process.env.REACT_APP_URL}/favorite/${id}`)
+      .then(response => {
+        if (response.status === 200) {
+          alert('Deleted')
+          window.location.reload();
+        }
+      })
+  }
+
+  modalShow=()=>{
+    this.setState ({
+      showModal:true 
+    })
+  }
+
+
+
+  closeModal=()=>{
+    this.setState ({
+      showModal: false ,
+      
+    })
+  }
+
   render() {
-    return(
+    return (
       <>
-        <h1>Fav Crypto List</h1>
+        {this.state.favArray.map((items, id) => {
+          return (
+            <>
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={items.image} />
+                <Card.Body>
+                  <Card.Title>{items.title}</Card.Title>
+                  <Card.Text>
+                    {items.description}
+                  </Card.Text>
+                  <Button variant="primary" onClick={this.deletefav(items)}>Delete</Button>
+                  <Button variant="primary" onClick={this.modalShow()}>Update</Button>
+                </Card.Body>
+              </Card>
+            </>
+          )
+        }
+
+        )
+
+        }
+
+        <Modal.Dialog onShow={this.state.showModal}  onHide={this.closeModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Update</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>
+            <Form onClick={this.updated()}>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Crypto Title</Form.Label>
+                <Form.Control type="text" onChange={this.updatehandle} defaultValue={this.state.title} />
+                <Form.Text className="text-muted">
+                  {this.state.description}
+                </Form.Text>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>toUSD</Form.Label>
+                <Form.Control type="text" onChange={this.updatehandle} defaultValue={this.state.toUSD}/>
+              </Form.Group>
+             
+              <Button variant="primary" type="submit" >
+              Save changes
+              </Button>
+            </Form>
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button variant="secondary">Close</Button>
+            
+          </Modal.Footer>
+        </Modal.Dialog>
       </>
     )
   }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,94 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import { Card, Button } from 'react-bootstrap'
 
 class Home extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      cryptoData: [] ,
+      title : '',
+      description : '' ,
+      toUSD : '' ,
+      image : '' ,
+    }
+  };
 
+
+  componentDidMount = () => {
+    let url = ('http://localhost:3001/Crypto')
+    axios.get(url).then(cryptoArray => {
+    
+      this.setState({
+        cryptoData: cryptoArray
+      })
+      console.log(this.state.cryptoData)
+    })
+  }
+
+
+
+  addtofav =(items)=>{
+    const { title , id , description , toUSD, image } = items ; 
+    // http://localhost:3001/favorite)
+
+    const requestconfig = {
+      method : 'post' ,
+      baseurl : process.env.REACT_APP_URL ,
+      url : '/favorite' ,
+      params :{
+        title: title,
+        description: description,
+        toUSD: toUSD,
+        image: image,
+        id: id
+      }
+
+    }
+    axios(requestconfig).then(response=>{
+      if(response.status === 200){
+        alert('done')
+      }
+    })
+
+
+  }
   render() {
+
+
     return (
       <>
-        <h1>Crypto List</h1>
+        {this.state.cryptoData.map(items => {
+          return (
+            <React.Fragment>
+              <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={items.image} />
+                <Card.Body>
+                  <Card.Title>{items.title}</Card.Title>
+                  <Card.Text>
+                    {items.description}
+                  </Card.Text>
+                  <Button variant="primary" onClick={this.addtofav(items)}>Add-To-watch-list</Button>
+                </Card.Body>
+              </Card>
+              </React.Fragment>
+          )
+        }
+
+        )
+
+        }
       </>
     )
+
+
+
+
+
+
   }
 }
 
 export default Home;
+
